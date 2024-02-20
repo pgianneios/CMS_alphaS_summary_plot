f052bb6307cdfc69d4c6ff0d0c10a3389f95fa46
gparaske <gianneios.paraskevas@cern.ch>
2024-02-20 18:29:41 +0100

    Style updates

--- SMP-23-004/alphaS_CMS.py
+++ SMP-23-004/alphaS_CMS.py
@@ -3,47 +3,48 @@ import numpy as np
 alpha_S = [
      # CMS determinations from Vector Boson cross sections
      { "val": np.array([0.1175]), "er_up": np.array([0.0025]), "er_dn": np.array([0.0028]), 
-       "obs": "W/Z cross sections", "sqrts": " (7, 8 TeV)", "ref": "JHEP 06:018", "year": "(2020)", "order": "NNLO" },
+       "obs": "W/Z cross sec.", "sqrts": "7, 8 ", "ref": "JHEP 06:018", "year": "(2020)", "order": "NNLO" },
 
      # CMS determinations from ttbar cross section
      { "val": np.array([0.1151]), "er_up": np.array([0.0033]), "er_dn": np.array([0.0032]), 
-       "obs": "t#bar{t} cross section", "sqrts": " (7 TeV)", "ref": "PLB 728:496", "year": "(2014)", "order": "NNLO" },
+       "obs": "t#bar{t} cross sec.", "sqrts": "7", "ref": "PLB 728:496", "year": "(2014)", "order": "NNLO" },
 
      { "val": np.array([0.1151]), "er_up": np.array([0.0040]), "er_dn": np.array([0.0035]), 
-       "obs": "t#bar{t} cross section", "sqrts": " (13 TeV)", "ref": "EPJC 79:368", "year": "(2019)", "order": "NNLO" },
+       "obs": "t#bar{t} cross sec.", "sqrts": "13", "ref": "EPJC 79:368", "year": "(2019)", "order": "NNLO" },
 
      { "val": np.array([0.1135]), "er_up": np.array([0.0021]), "er_dn": np.array([0.0017]),
-       "obs": "t#bar{t} cross section", "sqrts": " (13 TeV)", "ref": "EPJC 80:658", "year": "(2020)", "order": "NLO" },
+       "obs": "t#bar{t} cross sec.", "sqrts": "13", "ref": "EPJC 80:658", "year": "(2020)", "order": "NLO" },
  
      # CMS determinations from jet observables
      { "val": np.array([0.1148]), "er_up": np.array([0.0055]), "er_dn": np.array([0.0055]),
-       "obs": "R_{32}", "sqrts": " (7 TeV)", "ref": "EPJC 73:2604", "year": "(2013)", "order": "NLO" },
+       "obs": "R_{32}", "sqrts": "7", "ref": "EPJC 73:2604", "year": "(2013)", "order": "NLO" },
 
      { "val": np.array([0.1185]), "er_up": np.array([0.0063]), "er_dn": np.array([0.0042]),
-       "obs": "Inclusive jet", "sqrts": " (7 TeV)", "ref": "EPJC 75:288", "year": "(2015)", "order": "NLO" },
+       "obs": "Inclusive jet", "sqrts": "7", "ref": "EPJC 75:288", "year": "(2015)", "order": "NLO" },
 
      { "val": np.array([0.1171]), "er_up": np.array([0.0074]), "er_dn": np.array([0.0049]), 
-       "obs": "3-jet mass", "sqrts": " (7 TeV)", "ref": "EPJC 75:186", "year": "(2015)", "order": "NLO" },
+       "obs": "3-jet mass", "sqrts": "7", "ref": "EPJC 75:186", "year": "(2015)", "order": "NLO" },
  
      { "val": np.array([0.1164]), "er_up": np.array([0.0060]), "er_dn": np.array([0.0043]), 
-       "obs": "Inclusive jet", "sqrts": " (8 TeV)", "ref": "JHEP 03:156", "year": "(2017)", "order": "NLO" },
+       "obs": "Inclusive jet", "sqrts": "8", "ref": "JHEP 03:156", "year": "(2017)", "order": "NLO" },
  
      { "val": np.array([0.1199]), "er_up": np.array([0.0034]), "er_dn": np.array([0.0025]),
-       "obs": "Dijets (3D)", "sqrts": " (8 TeV)", "ref": "EPJC 77:746", "year": "(2017)", "order": "NLO" },
+       "obs": "Dijets (3D)", "sqrts": "8", "ref": "EPJC 77:746", "year": "(2017)", "order": "NLO" },
   
      { "val": np.array([0.1170]), "er_up": np.array([0.0019]), "er_dn": np.array([0.0019]),
-       "obs": "Inclusive jet", "sqrts": " (13 TeV)", "ref": "JHEP 02:142", "year": "(2022)", "order": "NNLO" },
+       "obs": "Inclusive jet", "sqrts": "13", "ref": "JHEP 02:142", "year": "(2022)", "order": "NNLO" },
 
      { "val": np.array([0.1179]), "er_up": np.array([0.0019]), "er_dn": np.array([0.0019]),
-       "obs": "Dijets (2D/3D)", "sqrts": " (13 TeV)", "ref": "arxiv:2312.16669", "year": "(2024)", "order": "NNLO" },
+       "obs": "Dijets (2D/3D)", "sqrts": "13", "ref": "arxiv:2312.16669", "year": "(2024)", "order": "NNLO" },
  
      { "val": np.array([0.1229]), "er_up": np.array([0.0040]), "er_dn": np.array([0.0050]),
-       "obs": "Energy correlators", "sqrts": " (13 TeV)", "ref": "CMS-PAS-SMP-22-015", "year": "(2024)", "order": "NNLL" },
+       "obs": "Energy correlators", "sqrts": "13", "ref": "CMS-PAS-SMP-22-015", "year": "(2024)", "order": "NNLL" },
  
      { "val": np.array([0.1177]), "er_up": np.array([0.0117]), "er_dn": np.array([0.0074]),
-       "obs": "R_{#Delta#phi}", "sqrts": " (13 TeV)", "ref": "CMS-PAS-SMP-22-005", "year": "(2024)", "order": "NLO" },
+       "obs": "R_{#Delta#phi}", "sqrts": "13", "ref": "CMS-PAS-SMP-22-005", "year": "(2024)", "order": "NLO" },
 
      # PDG 2023 world average
      { "val": np.array([0.1180]), "er_up": np.array([0.0009]), "er_dn": np.array([0.0009]),
-       "obs": "World avg", "sqrts": "", "ref": "Prog. Theor. Exp. Phys. 083C01", "year": "(2023 update)", "order": "" }
+       "obs": "            : World average", 
+       "sqrts": "", "ref": "Prog. Theor. Exp. Phys. 083C01", "year": "(2023 update)", "order": "" }
 ]
--- SMP-23-004/plot.py
+++ SMP-23-004/plot.py
@@ -14,7 +14,9 @@ PDG = 13
 # Create list with all graphs and legends
 Nvalues = len(alpha_S)
 graphs = []
-legends = []
+legends_ref = []
+legends_sqr = []
+legends_obs = []
 el = Nvalues + 1
 for a in alpha_S:
     # y values with no physical meaning only for drawing (Vector boson: upper, PDG: lower)
@@ -38,13 +40,14 @@ for a in alpha_S:
     
     gr.SetLineWidth(2)
     graphs.append(gr)
-    legends.append(a["obs"] + a["sqrts"] + " : " + a["ref"] + " " + a["year"])
+    legends_ref.append(a["ref"] + " " + a["year"])
+    legends_sqr.append(a["sqrts"])
+    legends_obs.append(a["obs"])
     el -= 1 # Index putting the measurements from top to bottom
 
 # Error band for PDG (only 1 point with PDG value and down y error the height of the histogram)
 grPDG = TGraphAsymmErrors(1, np.array([0.1180]), np.array([float(Nvalues+1.2)]), np.array([0.0009]), np.array([0.0009]), np.array([float(Nvalues+1.2)]), np.array([0.0]) )
-grPDG.SetFillColor(kYellow-7)
-grPDG.SetFillStyle(3001)
+grPDG.SetFillColorAlpha(kYellow+1, 0.5)
 
 # Different color style based on the category
 # WZ
@@ -63,13 +66,13 @@ graphs[PDG].SetMarkerColor(kBlack)
 graphs[PDG].SetLineColor(kBlack)
   
 # dummy histo/graph to make drawing easier
-h = TH1D("", "", 100, 0.088, 0.128)
+h = TH1D("", "", 100, 0.088, 0.127)
 dummy = TGraph(h)
 dummy.SetMinimum(1.0)
 dummy.SetMaximum(Nvalues + 3)
 dummy.GetYaxis().SetLabelSize(0.)
 dummy.GetYaxis().SetTickLength(0.)
-dummy.GetXaxis().SetTitle("#alpha_{s}(M_{Z}^{2})");
+dummy.GetXaxis().SetTitle("#alpha_{s}(M_{Z})");
 dummy.GetXaxis().SetTitleSize(0.04)
 dummy.GetXaxis().SetLabelSize(0.025)
 dummy.GetXaxis().SetTitleOffset(1.12)
@@ -80,42 +83,67 @@ dummy.Draw()
 grPDG.Draw("2 same") # This is the PDG band
 for g in graphs:
     g.Draw("P same")
+gPad.RedrawAxis()
 
-# Legends with observables and references
-xy = [ 0.005, 0.748,  0.4, 0.848 ]
-leg = []
-for l in range(len(legends)):
-    leg.append(TLegend(xy[0],xy[1],xy[2],xy[3]))
-    leg[l].AddEntry(legends[l],legends[l], "")
-    leg[l].SetTextFont(62)
-    leg[l].SetTextSize(0.018)
-    leg[l].SetFillColor(kWhite)
-    leg[l].SetLineColor(kWhite)
-    leg[l].SetBorderSize(0)
-    leg[l].SetFillStyle(0)
-    xy[1] -= 0.05
-    xy[3] -= 0.05
+# Legends with references, c.o.m. energy and observables
+xy_r = [ 0.005, 0.75,  0.4, 0.85 ]
+xy_s = [ 0.33, 0.75,  0.4, 0.85]
+xy_o = [ 0.49, 0.75,  0.4, 0.85]
+leg_r = []
+leg_s = []
+leg_o = []
+for l in range(len(legends_ref)):
+    leg_r.append(TLegend(xy_r[0],xy_r[1],xy_r[2],xy_r[3]))
+    leg_r[l].AddEntry(legends_ref[l],legends_ref[l], "")
+    leg_r[l].SetTextFont(62)
+    leg_r[l].SetTextSize(0.018)
+    leg_r[l].SetBorderSize(0)
+    leg_r[l].SetFillStyle(0)
+    xy_r[1] -= 0.05
+    xy_r[3] -= 0.05
+    leg_s.append(TLegend(xy_s[0],xy_s[1],xy_s[2],xy_s[3]))
+    leg_s[l].AddEntry(legends_sqr[l],legends_sqr[l], "")
+    leg_s[l].SetTextFont(62)
+    leg_s[l].SetTextSize(0.018)
+    leg_s[l].SetBorderSize(0)
+    leg_s[l].SetFillStyle(0)
+    xy_s[1] -= 0.05
+    xy_s[3] -= 0.05
+    leg_o.append(TLegend(xy_o[0],xy_o[1],xy_o[2],xy_o[3]))
+    leg_o[l].AddEntry(legends_obs[l],legends_obs[l], "")
+    leg_o[l].SetTextFont(62)
+    leg_o[l].SetTextSize(0.018)
+    leg_o[l].SetBorderSize(0)
+    leg_o[l].SetFillStyle(0)
+    xy_o[1] -= 0.05
+    xy_o[3] -= 0.05
 
 # Different color style based on the category
 # WZ
-leg[WZ].SetTextColor(kGreen+2)
+leg_r[WZ].SetTextColor(kGreen+2)
+leg_s[WZ].SetTextColor(kGreen+2)
+leg_o[WZ].SetTextColor(kGreen+2)
 # ttbar 
 for g in range(tt_first,tt_last+1):
-    leg[g].SetTextColor(kBlue+1)
+    leg_r[g].SetTextColor(kBlue+1)
+    leg_s[g].SetTextColor(kBlue+1)
+    leg_o[g].SetTextColor(kBlue+1)
 # Jets
 for g in range(jet_first,jet_last+1):
-    leg[g].SetTextColor(kRed+1)
+    leg_r[g].SetTextColor(kRed+1)
+    leg_s[g].SetTextColor(kRed+1)
+    leg_o[g].SetTextColor(kRed+1)
 
 # Draw all the legends
-for l in range(len(legends)):
-    leg[l].Draw()
+for l in range(len(legends_ref)):
+    leg_r[l].Draw()
+    leg_s[l].Draw()
+    leg_o[l].Draw()
 
 # Legends at the top showing the orders
 # NLO
 leg1 = TLegend(0.20,0.87,0.30,0.90)
 leg1.AddEntry(graphs[3]," ", "p")
-leg1.SetFillColor(kWhite)
-leg1.SetLineColor(kWhite)
 leg1.SetBorderSize(0)
 leg1.SetFillStyle(0)
 leg1.Draw()
@@ -124,8 +152,6 @@ leg2 = TLegend(0.22,0.87,0.35,0.90)
 leg2.AddEntry(graphs[4]," NLO", "p")
 leg2.SetTextSize(0.022)
 leg2.SetTextFont(62)
-leg2.SetFillColor(kWhite)
-leg2.SetLineColor(kWhite)
 leg2.SetBorderSize(0)
 leg2.SetFillStyle(0)
 leg2.Draw()
@@ -135,8 +161,6 @@ leg3 = TLegend(0.43,0.87,0.53,0.90)
 leg3.AddEntry(graphs[11],"  NNLL", "p")
 leg3.SetTextSize(0.022)
 leg3.SetTextFont(62)
-leg3.SetFillColor(kWhite)
-leg3.SetLineColor(kWhite)
 leg3.SetBorderSize(0)
 leg3.SetFillStyle(0)
 leg3.Draw()
@@ -144,16 +168,12 @@ leg3.Draw()
 # NNLO
 leg4 = TLegend(0.62,0.87,0.72,0.90)
 leg4.AddEntry(graphs[0]," ", "p")
-leg4.SetFillColor(kWhite)
-leg4.SetLineColor(kWhite)
 leg4.SetBorderSize(0)
 leg4.SetFillStyle(0)
 leg4.Draw()
 
 leg5 = TLegend(0.65,0.87,0.72,0.90)
 leg5.AddEntry(graphs[1]," ", "p")
-leg5.SetFillColor(kWhite)
-leg5.SetLineColor(kWhite)
 leg5.SetBorderSize(0)
 leg5.SetFillStyle(0)
 leg5.Draw()
@@ -162,8 +182,6 @@ leg6 = TLegend(0.68,0.87,0.72,0.90)
 leg6.AddEntry(graphs[10],"   NNLO", "p")
 leg6.SetTextSize(0.022)
 leg6.SetTextFont(62)
-leg6.SetFillColor(kWhite)
-leg6.SetLineColor(kWhite)
 leg6.SetBorderSize(0)
 leg6.SetFillStyle(0)
 leg6.Draw()
@@ -196,6 +214,45 @@ t3.SetTextSize(0.03)
 t3.SetTextColor(kRed+1)
 t3.SetTextAngle(270)
 t3.DrawLatexNDC(0.855, 0.40, "Jets")
+
+# Headers for the columns shown on the plot
+t4 = TLatex()
+t4.SetTextAlign(11)
+t4.SetTextSize(0.02)
+t4.SetTextColor(kBlack)
+t4.DrawLatexNDC(0.105, 0.82, "Reference")
+
+t5 = TLatex()
+t5.SetTextAlign(11)
+t5.SetTextSize(0.02)
+t5.SetTextColor(kBlack)
+t5.DrawLatexNDC(0.32, 0.82, "#sqrt{s} (TeV)")
+
+t6 = TLatex()
+t6.SetTextAlign(11)
+t6.SetTextSize(0.02)
+t6.SetTextColor(kBlack)
+t6.DrawLatexNDC(0.42, 0.82, "Observable")
+
+# CMS text + extra text
+tc = TText(0.0848,17.2,"CMS");
+tc.SetTextAlign(11);
+tc.SetTextFont(61);
+tc.SetTextSize(0.045);
+tc.Draw();
+
+te = TText(0.0905,17.2,"Preliminary");
+te.SetTextAlign(11);
+te.SetTextFont(52);
+te.SetTextSize(0.042);
+te.Draw();
+
+# Header top right
+tt = TLatex()
+tt.SetTextAlign(11)
+tt.SetTextSize(0.04)
+tt.SetTextColor(kBlack)
+tt.DrawLatexNDC(0.565, 0.922, "Summary of #alpha_{s}(M_{Z})")
 
 canv.Update()
 
